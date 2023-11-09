ae8e00842dd131b6a6f93978f094f48c6e2ae698
imrohituikey <rohituaike81@gmail.com>
2023-11-09 09:47:37 +0530

    signin page ui and functionality implemented

--- api/controllers/user.auth.js
+++ api/controllers/user.auth.js
@@ -1,5 +1,7 @@
-import bcrypt, { hashSync } from "bcrypt";
+import bcrypt, { hashSync,compareSync } from "bcrypt";
 import User from "../models/user.model.js";
+import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -17,6 +19,20 @@ export const signUp = async (req, res, next) => {
   }
 };
 
-export const signIn = async (req, res) => {
-  res.send({ message: "Hi from sign in" });
+export const signIn = async (req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email:email});
+    if(!validUser) return next(errorHandler(404, 'User not found!'));
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({id:validUser._id,}, process.env.JWT_SECRET);
+    const {password : pass, ...rest} = validUser._doc;
+    res
+      .cookie('access_token', token,{httpOnly : true})
+      .status(200)
+      .json(rest)
+  } catch (error) {
+    next(error)
+  }
 };
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -12,11 +12,29 @@ export default function Signin() {
       [e.target.id]: e.target.value,
     });
   };
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/user/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data)
+      navigate('/')
+    } catch (error) {
+      next(error)
+    }
+  }
   return (
     <div>
       <div className="flex flex-col text-center gap-8 p-12 xl:p-12">
         <h1 className="text-4xl font-bold">Login</h1>
         <form
+        onSubmit={handleSubmit}
           className="flex flex-col gap-4 sm:w-[35%] sm:mx-auto"
         >
          
